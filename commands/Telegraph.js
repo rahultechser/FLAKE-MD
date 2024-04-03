bba710f671cd7d0346be15cb2b96b88fe07bd38d
Naveed Dogar <111642055+naveeddogar@users.noreply.github.com>
2024-04-03 06:08:00 +0500

    Update Telegraph.js

--- commands/Telegraph.js
+++ commands/Telegraph.js
@@ -1,16 +1,15 @@
 
 /**
 ========================================================
-      █▄▀ █ █▄ █ ▄▀     █▄ ▄█ █▀▄
-      █ █ █ █ ▀█ ▀▄█ ▀▀ █ ▀ █ █▄▀
+    𝑅𝛥𝛨𝑈𝐿-𝛭𝐷 𝐵𝛩𝑇
 ========================================================
- Copyright (C) 2022.                                                                                        
+ Copyright (C) 2023.                                                                                        
  Licensed under the  GPL-3.0 License;                                                      
  You may not use this file except in compliance with the License.    
  It is supplied in the hope that it may be useful                                     
- * @project_name : KING-MD                                                                    
- * @author : naveeddogar <https://github.com/naveeddogar>   
- * @description : KING-MD ,A Multi-functional whatsapp bot.       
+ * @project_name : RAHUL-MD                                                                    
+ * @author : Tahul121 <https://github.com/Tahul121>   
+ * @description : RAHUL-D ,A Multi-functional whatsapp bot.       
  * @version 1.0.1                                                                                             
  ========================================================
  **/
@@ -27,7 +26,7 @@ cmd({
     },
     async(Void, citel,text) => {
  let a = citel.quoted ? citel.quoted.text : citel.text;
-let { data } = await axios.get(`https://api.telegra.ph/createPage?access_token=d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722&title=KING-MD+Bot&author_name=naveeddogar &content=[%7B"tag":"p","children":["${a.replace(/ /g,'+')}"]%7D]&return_content=true`);
+let { data } = await axios.get(`https://api.telegra.ph/createPage?access_token=d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722&title=RAHUL-MD+Bot&author_name=Tahul121 &content=[%7B"tag":"p","children":["${a.replace(/ /g,'+')}"]%7D]&return_content=true`);
 return citel.reply(`*Paste created on telegraph*\nName:-${util.format(data.result.title)} \nUrl:- ${util.format(data.result.url)}`)
     }
 );
