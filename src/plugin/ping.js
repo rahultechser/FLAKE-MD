b40515abc90f662972ef88b42b17921ca636a4fc
Rahul Debnath <137095636+rahultechser@users.noreply.github.com>
2024-10-05 15:43:36 +0530

    ✇⏤͟͞🕊️⃟𝗥𝗔𝗛𝗨𝗟 𝗧𝞢𝗖𝗛⬝✇⃪⃝➺

--- src/plugin/ping.js
+++ src/plugin/ping.js
@@ -7,11 +7,11 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
 
   if (cmd === "ping") {
     const start = new Date().getTime();
-    await m.React('⚡');
+    await m.React('📡');
     const end = new Date().getTime();
     const responseTime = (end - start) / 1000;
 
-    const text = `*_🔥⃝вσт ѕρєє∂: ${responseTime.toFixed(2)} s_*`;
+    const text = `*FLAKE MD PING ${responseTime.toFixed(2)} MS ⚙️*`;
     sock.sendMessage(m.from, { text }, { quoted: m });
   }
 }
